efca1c2e418d98a7b5b7bcd38214d44a5f169901
Pedro Teixeira <i@pgte.me>
2020-04-14 16:49:43 +0100

    increased the delays for tests

--- test/sync.test.js
+++ test/sync.test.js
@@ -39,7 +39,7 @@ describe('sync', () => {
     const usersLength = await getUsers()
     expect(usersLength).toBe(usersLength)
 
-    await delay(5000)
+    await delay(10000)
 
     const { body } = await elasticsearchClient.search(elasticGetAllUsers)
     expect( body.hits.total.value).toBe(usersLength)
@@ -48,7 +48,7 @@ describe('sync', () => {
   it('delete 50 users', async () => {
     await deleteUsers(50)
 
-    await delay(5000)
+    await delay(10000)
 
     const usersLength = await getUsers()
     const { body } = await elasticsearchClient.search(elasticGetAllUsers)
